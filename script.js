25744c4e5fd7edd8d1ce06cd73f72139703376ac
Andrew Talle <talle.andrew@gmail.com>
2020-11-11 19:15:09 -0800

    setup project

---
+++ script.js
@@ -0,0 +1,27 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sort = document.getElementById('sort');
+const calculateWealth = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+
+// fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+  
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  }
+  console.log(newUser)
+}
